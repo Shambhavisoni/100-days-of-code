2cb811e8d03b7179dd814897a6d20042f7a16183
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2022-12-31 08:39:00 +0530

    Update Missing number in array(GFG).cpp

--- Day 2/Missing number in array(GFG).cpp
+++ Day 2/Missing number in array(GFG).cpp
@@ -11,6 +11,24 @@ class Solution{
         return n;
     }
 };
+/*TC: O(nlogn)
+SC: O(1)*/
+
+
+/*Approach 2 (Using binary operations): This method uses the technique of XOR to solve the problem.  
+
+XOR has certain properties 
+
+Assume a1 ⊕ a2 ⊕ a3 ⊕ . . . ⊕ an = a and a1 ⊕ a2 ⊕ a3 ⊕ . . . ⊕ an-1 = b
+Then a ⊕ b = an
+Follow the steps mentioned below to implement the idea:
+
+Create two variables a = 0 and b = 0
+Run a loop from i = 1 to N:
+For every index, update a as a = a ^ i
+Now traverse the array from i = start to end.
+For every index, update b as b = b ^ arr[i].
+The missing number is a ^ b.*/
 
 class Solution{
   public:
@@ -26,3 +44,43 @@ class Solution{
         return (x1^x2);
     }
 };
+/*TC: O(n)
+SC: O(1)*/
+
+/*Approach 3 (Using Cyclic Sort): The idea behind it is as follows:
+
+All the given array numbers are sorted and in the range of 1 to n-1. If the range is 1 to N  then the index of every array element will be the same as (value - 1).
+
+Follow the below steps to implement the idea:
+
+Use cyclic sort to sort the elements in linear time.
+Now traverse from i = 0 to the end of the array:
+If arr[i] is not the same as i+1 then the missing element is (i+1).
+If all elements are present then N is the missing element in the range [1, N].
+Below is the implementation of the above approach.*/
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int i=0;
+        while(i<n){                  //used cylic sort instead of build in sort method as cyclic sort complexcity-O(n)
+            int correct=array[i]-1;
+            if(array[i]<n && array[i]!=array[correct]){
+                swap(array[i],array[correct]);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(array[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n;
+    }
+};
+
+/*TC: O(n)
+SC: O(1)*/
